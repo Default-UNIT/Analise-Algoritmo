4ebe246c020266656977b00a0a5ec285ea60fe36
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2021-03-10 14:54:57 -0300

    Update BuscaLinear.java

--- Exercicios 09.03/BuscaLinear.java
+++ Exercicios 09.03/BuscaLinear.java
@@ -5,15 +5,19 @@ public class BuscaLinear {
     public static void linearSearchNumber(int[] listaNum, int numDigitado) {
         int i = 0;
 
-        while(listaNum[i] != numDigitado) {
-            i++;
+        while(true) {
+            if (numDigitado == listaNum[i]) {
+                JOptionPane.showMessageDialog(null, "O número " + numDigitado + " está na posição: " + i);
+                break;
+            } else if(numDigitado != listaNum[i]) {
+                i++;
+            }
         }
-        if(listaNum[i] == numDigitado){
-            JOptionPane.showMessageDialog(null, "O número "+ numDigitado + " está na posição: " + i);
-        } else {
-            JOptionPane.showMessageDialog(null, "O número " + numDigitado + " não foi encontrado na Busca");
-        }
+
+        JOptionPane.showMessageDialog(null, "O número " + numDigitado + " não foi encontrado na Busca");
+
     }
+
 
     public static void linearSearchString(String[] listaString, String strDigitada) {
         int i = 0;
@@ -23,12 +27,10 @@ public class BuscaLinear {
         }
 
         if(listaString[i].equals(strDigitada)) {
-            JOptionPane.showMessageDialog(null, "A palavra "+ strDigitada + " está na posição: " + i);
-        } else {
-            JOptionPane.showMessageDialog(null, "A palavra " + strDigitada + " não foi encontrado na Busca");
+            JOptionPane.showMessageDialog(null, "A palavra '"+ strDigitada + "' está na posição: " + i);
         }
-
     }
+
 
     public static void main(String[] args) {
         int opc;
@@ -37,13 +39,13 @@ public class BuscaLinear {
         String[] listaString = new String[5];
         Object[] options = {"Número", "String"};
 
-        opc = JOptionPane.showOptionDialog(null, "Escolha o tipo de lista que deseja criar:", "Panelinha",
+        opc = JOptionPane.showOptionDialog(null, "Escolha o tipo de lista que deseja criar:", "Busca Linear",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.DEFAULT_OPTION, null, options, options[0]);
 
         switch (opc){
             case 0:
 
-                for(int i = 0; i < 5; i++) {
+                for(int i = 0; i < listaNum.length; i++) {
                     listaNum[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite o " + (i+1) +"/5 número: "));
                 }
                 int numDigitado = Integer.parseInt(JOptionPane.showInputDialog("Qual número deseja Buscar: "));
@@ -54,7 +56,7 @@ public class BuscaLinear {
 
             case 1:
 
-                for(int i = 0; i < 5; i++) {
+                for(int i = 0; i < listaString.length; i++) {
                     listaString[i] = JOptionPane.showInputDialog("Digite a " + (i+1) +"/5 palavra: ");
                 }
                 String strDigitada = JOptionPane.showInputDialog("Qual palavra deseja Buscar: ");
